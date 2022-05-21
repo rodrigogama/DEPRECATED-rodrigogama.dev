6cf429d707a29dc96bf3fcff3f69d97748a4289c
Rodrigo Gama <ro.gama91@gmail.com>
2022-05-21 15:24:39 +0200

    Update social.ts

--- constants/social.ts
+++ constants/social.ts
@@ -5,7 +5,7 @@ export const SOCIAL_LINKS: SocialLinks = [
   { name: 'Github', url: 'https://github.com/rodrigogama' },
   {
     name: 'Linkedin',
-    url: 'https://www.linkedin.com/in/rodrigo-gama-baptista/',
+    url: 'https://www.linkedin.com/in/rodrigogamadev/',
   },
   { name: 'Instagram', url: 'https://www.instagram.com/rodrigogama.dev/' },
   { name: 'Medium', url: 'https://medium.com/@rodrigogamadev' },
